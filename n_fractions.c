4a184e67a8542476e3d5de707122817260d857f9
KevinHenry21 <69193462+KevinHenry21@users.noreply.github.com>
2021-02-11 22:49:48 +0530

    Update n_fractions.c

--- n_fractions.c
+++ n_fractions.c
@@ -1,1 +1,77 @@
 //WAP to find the sum of n fractions.
+#include<stdio.h>
+struct fractions
+{
+	int num, den;
+};
+
+int gcd(int a, int b)
+{
+	int gcd;
+	for(int i=1; i <= a && i <= b; ++i)
+    {
+        if(a % i == 0 && b % i == 0)
+            gcd = i;
+    } 
+    return gcd;
+}
+
+struct fractions input()
+{
+    struct fractions a;
+    
+    scanf("%d%d", &a.num, &a.den);
+    return a;
+}
+
+void frac_addition(int n, struct fractions a[])
+{
+    struct fractions b;
+    int res_num = 0;
+    int res_den = a[0].den;
+    for (int i = 1; i < n; i++) 
+    {
+        res_den = (((a[i].den * res_den)) / (gcd(a[i].den, res_den)));
+    }
+    
+    for (int i = 0; i < n; i++) 
+    {
+        res_num = res_num + (a[i].num) * (res_den / a[i].den);
+    }
+    
+    int GCD = gcd(res_num, res_den);
+    
+    b.num = res_num / GCD;
+    b.den = res_den / GCD;
+    
+    output(a, b, n);
+}
+
+void output(struct fractions a[], struct fractions b, int n)
+{
+    printf("The sum of ");
+    for(int i = 0; i<n ; i++)
+    {
+        printf("%d/%d + ", a[i].num, a[i].den);
+    }
+    printf(" is %d/%d \n", b.num, b.den);
+}
+
+int main()
+{
+    struct fractions a[100];
+    int n;
+    
+    printf("Enter n: ");
+    scanf("%d", &n);
+    
+    for(int i = 0; i<n; i++)
+    {
+        printf("Enter fraction %d: ", i+1);
+        a[i] = input();
+    }
+    
+    frac_addition(n, a);
+
+    return 0;
+}
